51da0309e4114a66eb044787bec0090e3db269d8
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-02 17:52:49 +0100

    Fixed names of components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./containers/header";
-import productListing from "./containers/productListing";
-import productDetail from "./containers/productDetail";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" exact component={productListing} />
-          <Route path="/product/:productId" exact component={productDetail} />
+          <Route path="/pro" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
           <Route>404 Not Found!</Route>
         </Routes>
       </Router>
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-const productListing = () => {
+const ProductListing = () => {
   const products = useSelector((state) => state);
   console.log(products);
   return (
     <div className="ui grid container">
-      <h1>productListing</h1>
+      <h1>ProductListing</h1>
     </div>
   );
 };
 
-export default productListing;
+export default ProductListing;
